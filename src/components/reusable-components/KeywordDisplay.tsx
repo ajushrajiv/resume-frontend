cce268b44d2ac0ec1e692ada8dd3a3fb18efdf97
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-06 20:45:32 +0200

    keyword page

--- src/components/reusable-components/KeywordDisplay.tsx
+++ src/components/reusable-components/KeywordDisplay.tsx
@@ -3,9 +3,21 @@
 import React from 'react';
 
 const KeywordDisplay: React.FC<KeywordDisplayProps> = ({ keywords }) => {
+
+  const keywordArray = keywords.split(' ');
+
+  const getRandomColor = () => {
+    const colors = ['#75BDE0', '#345DA7', '#7339AB', '#028174'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
   return (
-    <div className=" whitespace-pre-wrap leading-loose block w-full text-sky-600 h-96 bg-gray-50 text-text-blue text-lg tracking-wide border border-gray-500 rounded pt-4 px-12 mb-3 focus:outline-none focus:bg-white overflow-auto">
-      {keywords}
+    <div className=" whitespace-pre-wrap leading-loose block w-full text-sky-600 bg-gray-50 text-text-blue text-lg tracking-wide border border-gray-500 rounded pt-4 px-12 mb-3 focus:outline-none focus:bg-white overflow-auto">
+      {keywordArray.map((keyword, index) => (
+        <span key={index} style={{ color: getRandomColor() }}>
+          {keyword}{' '}
+        </span>
+      ))}
     </div>
   );
 }
--- src/components/reusable-components/TextArea.tsx
+++ src/components/reusable-components/TextArea.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const TextArea: React.FC<TextAreaProps> = ({ name, value, onChange, placeholder }) => {
   return (
     <textarea
-        className="appearance-none block w-full h-96 bg-gray-50 text-black border border-gray-500 rounded py-3 px-12 mb-3 leading-tight focus:outline-none focus:bg-white text-left overflow-auto"
+        className="appearance-none block w-full h-80 bg-gray-50 text-black border border-gray-500 rounded py-3 px-12 mb-3 leading-tight focus:outline-none focus:bg-white text-left overflow-auto"
         placeholder={placeholder}
         name={name}
         value={value}
